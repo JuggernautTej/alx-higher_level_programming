b8365564d696b7608cff2a594c20ea0e9bfd9aba
JuggernautTej <tiwatej@gmail.com>
2024-01-07 05:41:36 -0800

    added task 6 file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        index = 0
+        for y in x:
+            if index < len(x) - 1:
+                print("{:d}".format(y), end=' ')
+                index += 1
+            else:
+                print("{:d}".format(y), end='')
+        print()
