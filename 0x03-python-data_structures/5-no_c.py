2e82e578948f98eb66f9e401b821af3c9ee6f6a5
JuggernautTej <tiwatej@gmail.com>
2024-01-07 03:47:52 -0800

    added no_c file

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    str_list = list(my_string)
+    chr_to_remove = ["c", "C"]
+    for chr in chr_to_remove:
+        str_list = [str for str in str_list if str != chr]
+    return ''.join(str.list)
