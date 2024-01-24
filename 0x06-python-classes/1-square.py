c901eb57ff9996f52448d8e1854abbd9411f5851
JuggernautTej <tiwatej@gmail.com>
2024-01-23 17:33:23 -0800

    added task 1 file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This class takes  care of square functions"""
+
+
+class Square:
+    """This class has a private field entry."""
+    def __init__(self, size):
+        """
+        Initializes the data
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
