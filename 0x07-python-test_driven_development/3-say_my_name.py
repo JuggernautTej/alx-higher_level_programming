81ffc5964063c7e5df9bd5e1889182ec03fd074e
JuggernautTej <tiwatej@gmail.com>
2024-01-29 04:31:28 -0800

    added task 2 files. This includes the say_my_name module and the doctest text file for the module

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This function say_my_name takes in two arguments, strings
+and prints them.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This function prints two strings in a sentence.
+    Args:
+    first_name (string): The first parameter
+    last_name (string): The second parameter
+    
+    Returns:
+    Nothing.
+    """
+    if not(isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    if not(isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
