7eb0e342a742ed723b1da36c130a8f98d383bd5c
JuggernautTej <tiwatej@gmail.com>
2024-01-29 16:02:40 -0800

    created new directory to house the more more classes project. added Tom and Jerry

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This class defines a rectangle but it is an empty class"""
+
+
+class Rectangle:
+    """This is an empty class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# MORE PYTHON CLASSES
+This directory houses the tasks of the OOP project part 2.
