86cdd116a041d352c61b755320becde6f4ce26ac
JuggernautTej <tiwatej@gmail.com>
2024-02-08 02:38:49 -0800

    edits and additions

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is class validates the values of a geometry"""
+
+
+class BaseGeometry:
+    """This is the geometry class."""
+    def __init__(self):
+        """This initializes the class"""
+        pass
+    def area(self):
+        """This method raises an exceptio"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """This method valideates the value of the geometry"""
+        if not (isinstance(value, int)):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
