6434399a9851dfa19f646fa04177ed0fc5111779
JuggernautTej <tiwatej@gmail.com>
2024-04-16 19:02:19 +0100

    edited task 4 python file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,26 +10,12 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                             passwd=mysql_password, db=db_name, charset="utf8")
-    except MySQLdb.Error as err:
-        print("Error connecting to database: {}".format(err))
-        sys.exit(1)
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=db_name, charset="utf8")
     cur = db.cursor()
-    try:
-        cur.execute(
-            "SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.id = states.id\
-            ORDER BY cities.id ASC")
-        rows = cur.fetchall()
-        if rows is not None:
-            for line in rows:
-                print(line)
-    except MySQLdb.Error as err:
-        print("Error executing query: {}".format(err))
-        sys.exit(1)
-    finally:
-        cur.close()
-        db.close()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities INNER JOIN states ON cities.state_id = states.id\
+    ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for line in rows:
+        print(line)
