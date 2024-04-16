17eb0e0218abf46e2e6ead4a2924c8ec316e53c9
JuggernautTej <tiwatej@gmail.com>
2024-04-16 18:04:04 +0100

    added task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                             passwd=mysql_password, db=db_name, charset="utf8")
+    except MySQLdb.Error as err:
+        print("Error connecting to database: {}".format(err))
+        sys.exit(1)
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC",
+        (state_name,))
+    rows = cur.fetchall()
+
+    for line in rows:
+        print(line)
+
+    cur.close()
+    db.close()
