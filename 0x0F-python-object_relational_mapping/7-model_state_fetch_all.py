56c3e7835f3beaf400eb23c1dce86b3d505782bd
JuggernautTej <tiwatej@gmail.com>
2024-04-20 12:49:34 +0100

    updated the documentation on task 7 file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,15 +8,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    """Test documentation"""
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-# Created the SQLAlchemy engine
 db = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, db_name)
 engine = create_engine(db, echo=True)
-# Created a session
 Session = sessionmaker(bind=engine)
-
 session = Session()
 # Query to retrieve all State objects from database
 for objects in session.query(State).order_by(State.id):
