e75e34269429e28cd1624ca09094322d86b643ae
JuggernautTej <tiwatej@gmail.com>
2024-04-25 17:17:44 +0100

    updated task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # This Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+[[ $(curl -s -o /dev/null -w "%{http_code}" "$1") == 200 ]] && curl -s "$1" | sed '$d'
